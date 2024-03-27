1ce8ac37c0085c4aef784c16b67ba25cfdacc0ac
Asthen <asthen@infotree.net.my>
2024-03-27 16:06:24 +0800

    origin

--- ikramqa/settings_prod.py
+++ ikramqa/settings_prod.py
@@ -201,7 +201,6 @@ STORAGES = {
         "BACKEND": "storages.backends.azure_storage.AzureStorage",
         "OPTIONS": {
             "azure_container": 'media',
-            # "connection_string": 'SharedAccessSignature=sv=2023-01-03&ss=btqf&srt=sco&st=2024-03-08T05%3A59%3A00Z&se=2025-03-09T05%3A59%3A00Z&sp=rwl&sig=q5znqFlJkpdCbKUz8rFiGt3HCXDA7C18a4vfjMgj87Y%3D;BlobEndpoint=https://ikramtest.blob.core.windows.net/;FileEndpoint=https://ikramtest.file.core.windows.net/;QueueEndpoint=https://ikramtest.queue.core.windows.net/;TableEndpoint=https://ikramtest.table.core.windows.net/;',
             "account_key": config('ACCOUNT_KEY'),
             "account_name": "ikramqa",
             "overwrite_files" : True,
@@ -213,8 +212,7 @@ STORAGES = {
             "azure_container": 'static',
             "account_key": config('ACCOUNT_KEY'),
             "account_name": "ikramqa",
-            # "connection_string": 'SharedAccessSignature=sv=2023-01-03&ss=btqf&srt=sco&st=2024-03-08T05%3A59%3A00Z&se=2025-03-09T05%3A59%3A00Z&sp=rwl&sig=q5znqFlJkpdCbKUz8rFiGt3HCXDA7C18a4vfjMgj87Y%3D;BlobEndpoint=https://ikramtest.blob.core.windows.net/;FileEndpoint=https://ikramtest.file.core.windows.net/;QueueEndpoint=https://ikramtest.queue.core.windows.net/;TableEndpoint=https://ikramtest.table.core.windows.net/;',
-        }
+            }
     },
 }
 
