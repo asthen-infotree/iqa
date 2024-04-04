863508b5d692435b8d26ea8254fa50bf365fb2e9
asthen-infotree <165005741+asthen-infotree@users.noreply.github.com>
2024-04-04 23:36:11 +0800

    Update admin.py

--- certificate/admin.py
+++ certificate/admin.py
@@ -13,6 +13,9 @@ from django_summernote.admin import SummernoteModelAdmin
 from certificate.models import Certificate, Standards, Brand, ProductDescription, Product
 from ikramqa import settings
 from certificate.views import makeWatermark, render_pdf_view
+from django.db.migrations.recorder import MigrationRecorder
+from django.contrib import admin
+
 
 
 # Register your models here.
@@ -208,3 +211,4 @@ class BrandSiteAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Brand, BrandSiteAdmin)
+admin.site.register(MigrationRecorder.Migration)
