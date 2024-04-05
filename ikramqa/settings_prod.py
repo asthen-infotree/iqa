79b0e2081423f2e1aa8619bc3ec922091ff00760
Asthen <asthen@infotree.net.my>
2024-04-05 15:54:29 +0800

    fix database to psql

--- ikramqa/settings_prod.py
+++ ikramqa/settings_prod.py
@@ -115,23 +115,23 @@ WSGI_APPLICATION = 'ikramqa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': os.getenv('DBNAME'),
-#             'USER': os.getenv('DBUSER'),
-#             'PASSWORD': os.getenv('DBPASS'),
-#             'HOST': os.getenv('DBHOST'),
-#             'PORT': '5432',
-#         }
-#     }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.getenv('DBNAME'),
+            'USER': os.getenv('DBUSER'),
+            'PASSWORD': os.getenv('DBPASS'),
+            'HOST': os.getenv('DBHOST'),
+            'PORT': '5432',
+        }
     }
-}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'db.sqlite3',
+#     }
+# }
 
 AUTH_USER_MODEL = 'users.CustomUser'
 # Password validation
