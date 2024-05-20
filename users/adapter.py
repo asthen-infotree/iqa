b8bf9dd1e147dc4f9fbfcdd1538e236037ff8387
Asthen <asthen@infotree.net.my>
2024-05-20 09:27:25 +0800

    added adapter

--- users/adapter.py
+++ users/adapter.py
@@ -1,3 +1,4 @@
+from allauth.core.exceptions import ImmediateHttpResponse
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.http import HttpResponse
 from django.http import Http404
@@ -5,7 +6,8 @@ from django.http import Http404
 
 class MySocialAccount(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
-        u = sociallogin.account.user
-        print(not u.email.split('@')[1] == "ikram.com.my")
-        if not u.email.split('@')[1] in ["infotree.net.my", "ikram.com.my"]:
-            raise Http404
+        email_domain = sociallogin.user.email.split('@')[1].lower()
+        if not email_domain in ["infotree.net.my", "ikram.com.my"]:
+            raise ImmediateHttpResponse(HttpResponse(sociallogin.user.email + ' is not valid memeber of ikram.com.my'))
+        else:
+            pass
