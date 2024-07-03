fa3005c813b1b6df8ca5423628ecb74835e63987
Asthen <asthen@infotree.net.my>
2024-07-03 14:32:06 +0800

    added download feature for feedback section

--- frontend_settings/admin.py
+++ frontend_settings/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
+from django.shortcuts import redirect
+from django.urls import reverse
+
 from frontend_settings.models import Feedback, Banner
 
 # Register your models here.
-admin.site.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    def response_change(self, request, obj):  # Here
+        if "_custom_button" in request.POST:
+            # return render_pdf_view(request, obj)
+            return redirect(reverse('feedback_download', kwargs={'feedback_id': obj.id}))
+
+            # Do something
+            # return super().response_change(request, obj)
+            # return HttpResponseRedirect(".")
+        else:
+            # print('123123')
+            # Do something
+            return super().response_change(request, obj)
+
+admin.site.register(Feedback, FeedbackAdmin)
+
+
+
 admin.site.register(Banner)
