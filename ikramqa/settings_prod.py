387adfa7dfa54a08dd4d3a057ae3faef0dcb6dde
Asthen <asthen@infotree.net.my>
2024-08-06 15:15:05 +0800

    csrf trusted origins update

--- ikramqa/settings_prod.py
+++ ikramqa/settings_prod.py
@@ -90,7 +90,7 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-CSRF_TRUSTED_ORIGINS = ['https://ikramqa.azurewebsites.net']
+CSRF_TRUSTED_ORIGINS = ['https://ikramqa.azurewebsites.net', 'https://ikramqa.com.my']
 
 ROOT_URLCONF = 'ikramqa.urls'
 
